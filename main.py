8326d62937d357da9abbda24efd0ac5a28e9463d
MonnXI <123272198+MonnXI@users.noreply.github.com>
2024-01-29 21:00:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
-
-os.system("pip install pygame")
-os.system("pip install asyncio")
-
-import pygame
 import random
-import asyncio
 
+try:
+	import pygame
+	import asyncio
+except ImportError:
+	print("___Downloading and verifying packages___")
+	yn = input("___Do you want to download them [Y/n]___")
+	if yn.lower() == "y":
+		os.system("pip install pygame")
+		os.system("pip install asyncio")
+	else:
+		print("Abort quitting the programm")
